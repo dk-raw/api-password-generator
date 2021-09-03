9cc5a0a5721f7f4442873109daacd4e794c068a7
dk-raw <ddkatsios@protonmail.com>
2021-09-03 20:42:24 +0300

    added more special characters in the charset

--- routes/generate.js
+++ routes/generate.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const generatePassword = (length) => {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=<>,./?\|"
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=<>,./?\|[]{};:`~"
     let password = '';
     for (let i = 0, n = charset.length; i < length; ++i) {
         password += charset.charAt(Math.floor(Math.random() * n));
