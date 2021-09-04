dba1e80a55a75395e1c63c8b768a3d83f5a6ab62
dk-raw <ddkatsios@protonmail.com>
2021-09-04 11:20:13 +0300

    added instructions

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     const current = new Date();
     const time = current.toLocaleTimeString("en-US");
-    res.send(`time in the server: ${time}`);
+    res.send(`Time in the server: ${time}. <br />
+        To generater a pasword, GET /generate/{password length}. <br /> GUI version: https://password-generator-express.herokuapp.com/`);
 });
 
 const generateRouter = require('./routes/generate');
