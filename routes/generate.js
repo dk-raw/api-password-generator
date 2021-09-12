e481e5708b552ab57833ca8f7018c0dd099a51cd
dk-raw <ddkatsios@protonmail.com>
2021-09-12 18:03:47 +0300

    fixed types and error handling

--- routes/generate.js
+++ routes/generate.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const MAX_LENGTH = 99999;
+const MIN_LENGTH = 2;
 
 const generatePassword = (length) => {
     const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=<>,./?\|[]{};:`~"
@@ -11,10 +13,14 @@ const generatePassword = (length) => {
 }
 
 router.get('/:length', (req, res) => {
-    res.json({
-        password: generatePassword(req.params.length),
-        length: req.params.length
-    });
+    if (isNaN(req.params.length) || req.params.length < MIN_LENGTH || req.params.length > MAX_LENGTH || (Math.floor(req.params.length) != req.params.length)) {
+        res.status(400).send(`Bad Request: length value ${req.params.length} is invalid.`);
+    } else {
+        res.json({
+            password: generatePassword(parseFloat(req.params.length)),
+            length: parseFloat(req.params.length)
+        });
+    }
 });
 
 module.exports = router;
